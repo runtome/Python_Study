3946b1e7f209b63e802068c89981864f8070f574
SUPHOT NARAPONG <suphot.n@gmail.com>
2025-05-13 21:36:44 +0700

    variable

--- main.py
+++ main.py
@@ -12,8 +12,20 @@
 
 
 #integer
-a = 5
-b = 10
-c = a + b
-print(c)
-print(type(c))
+# a = 5
+# b = 10
+# c = a + b
+# print(c)
+# print(type(c))
+
+
+#Variable
+year = 2023
+print(year)
+print(type(year))
+
+user_name = "John Doe"
+print(user_name)
+
+my_age = 25
+print(my_Age)
